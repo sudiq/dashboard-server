cdbc8d782c0cade850776a1a68b636fd4ffc70a3
sadiq aderinto <sadiq@gmail.com>
2021-01-07 08:33:25 +0100

    added user model

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, Request, Response
 from flask_restful import Api
+from flask_bcrypt import Bcrypt
 from resources.routes import initroutes
 
 from database.db import initdb
@@ -9,6 +10,11 @@ import json
 
 app = Flask(__name__)
 api = Api(app)
+bcrypt = Bcrypt(app)
+
+app.config['ENV'] = 'development'
+app.config['DEBUG'] = True
+app.config['TESTING'] = True
 app.config['MONGO_SETTINGS'] = {
     'host': 'mongodb://localhost/movie-bag'
 }
--- database/models.py
+++ database/models.py
@@ -1,8 +1,16 @@
 from .db import db
+from flask_bcrypt import generate_password_hash, check_password_hash
 
 class Discos(db.Document):
-    name = db.StringField(required= True, unique = True)
+    name = db.StringField(required = True, unique = True)
     Feeder_33 = db.ListField(db.StringField())
     Feeder_11 = db.ListField(db.StringField())
 
-    
+class Users(db.Document):
+    email = db.StringField(require = True, unique = True)
+    password = db.StringField(required = True)
+    disco  = db.ReferencField('Discos')
+    def hash_pass(self):
+        self.password = generate_password_hash(self.password)
+    def chcek_pass(self, password):
+        return check_password_hash(self.password, password)
